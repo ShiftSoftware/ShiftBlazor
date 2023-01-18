4bb5c3c78f8c21ce6039cdc0d249b1913d69a54c
Saman <gtsaman@gmail.com>
2023-01-18 13:27:11 +0300

    #86775u553: Custom MainLayout

---
+++ ShiftBlazor/Utils/ShiftMainLayout.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Components.Routing;
+using Microsoft.AspNetCore.Components;
+using ShiftSoftware.ShiftBlazor.Services;
+
+namespace ShiftSoftware.ShiftBlazor.Utils
+{
+    public class ShiftMainLayout : LayoutComponentBase
+    {
+        [Inject] ShiftModalService ShiftModal { get; set; } = default!;
+        [Inject] NavigationManager NavManager { get; set; } = default!;
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+
+            NavManager.LocationChanged += HandleLocationChanged;
+            OpenModals();
+        }
+
+        private void HandleLocationChanged(object? sender, LocationChangedEventArgs e)
+        {
+            OpenModals();
+        }
+
+        private void OpenModals()
+        {
+            ShiftModal.UpdateModals();
+        }
+    }
+}
