99aa92a7a4c4989c63e426480d2b18fde2e53385
Saman <gtsaman@gmail.com>
2023-01-29 20:15:44 +0300

    #867793zkf: Create a method to add all services

---
+++ ShiftBlazor/Extensions/ServiceExtension.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MudBlazor;
+using MudBlazor.Services;
+using ShiftSoftware.ShiftBlazor.Services;
+using Syncfusion.Licensing;
+using Syncfusion.Blazor;
+
+namespace ShiftSoftware.ShiftBlazor.Extensions
+{
+    public static class ServiceExtension
+    {
+        public static IServiceCollection AddShiftServices(this IServiceCollection services, Action<ShiftBlazorOptions> configure)
+        {
+            ShiftBlazorOptions options = new ShiftBlazorOptions();
+            configure.Invoke(options);
+
+            services.AddMudServices(mudConfig =>
+            {
+                mudConfig.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomRight;
+                mudConfig.SnackbarConfiguration.ShowCloseIcon = true;
+                mudConfig.SnackbarConfiguration.VisibleStateDuration = 10000;
+                mudConfig.SnackbarConfiguration.HideTransitionDuration = 250;
+                mudConfig.SnackbarConfiguration.ShowTransitionDuration = 250;
+                mudConfig.SnackbarConfiguration.SnackbarVariant = Variant.Text;
+                mudConfig.SnackbarConfiguration.BackgroundBlurred = false;
+                mudConfig.SnackbarConfiguration.PreventDuplicates = false;
+                mudConfig.SnackbarConfiguration.MaxDisplayedSnackbars = 5;
+
+                options.MudBlazorConfiguration.Invoke(mudConfig);
+            });
+
+            services.AddScoped<ClipboardService>();
+            services.AddScoped<ShiftModalService>();
+            services.AddScoped<MessageService>();
+            services.AddScoped(sp => new ODataQuery(options.ODataEndpoint));
+
+            services.AddSyncfusionBlazor(options.SyncfusionConfiguration.Invoke);
+            SyncfusionLicenseProvider.RegisterLicense(options.SyncfusionLicense);
+
+            return services;
+        }
+
+        public class ShiftBlazorOptions
+        {
+            public string ODataEndpoint { get; set; }
+            public string? SyncfusionLicense { get; set; }
+            public Action<MudServicesConfiguration> MudBlazorConfiguration { get; set; }
+            public Action<GlobalOptions> SyncfusionConfiguration { get; set; }
+        }
+    }
+}
