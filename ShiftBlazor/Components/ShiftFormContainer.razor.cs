6716e98ac6809a3658423cb6ff235175d8b0c9f7
Aza Asim <azaasim@gmail.com>
2023-02-05 18:44:49 +0300

    Navigate after submit: ignore if ID is an empty GUID (2)

--- ShiftBlazor/Components/ShiftFormContainer.razor.cs
+++ ShiftBlazor/Components/ShiftFormContainer.razor.cs
@@ -468,7 +468,9 @@ namespace ShiftSoftware.ShiftBlazor.Components
                         if (Item != null)
                         {
                             await KeyChanged.InvokeAsync(Item.ID);
-                            UpdateUrl(Item.ID);
+
+                            if (Item.ID != default)
+                                UpdateUrl(Item.ID);
                         }
                         await OnEditResponse.InvokeAsync(shiftEntityResponse);
                     }
@@ -488,7 +490,7 @@ namespace ShiftSoftware.ShiftBlazor.Components
 
         private void UpdateUrl(object key)
         {
-            if (Value.ID != default || key == default)
+            if (Value.ID != default)
                 return;
 
             if (MudDialog == null)
