92dff8c6c8d45dad7e47fa106ccd33ff56c116ac
Saman <gtsaman@gmail.com>
2023-02-22 15:05:44 +0300

    #8677fqzck: Add ShiftFormBasic unit tests

--- ShiftBlazor.Tests/SampleItem.cs
+++ ShiftBlazor.Tests/SampleItem.cs
@@ -1,4 +1,5 @@
-﻿using ShiftSoftware.ShiftEntity.Core.Dtos;
+﻿using FluentValidation;
+using ShiftSoftware.ShiftEntity.Core.Dtos;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,5 +15,19 @@ namespace ShiftSoftware.ShiftBlazor.Tests
         public int Age { get; set; }
         public string City { get; set; }
         public int Revisions { get; set; }
+    }
+
+    public class SampleValidator : AbstractValidator<Sample>
+    {
+        public SampleValidator()
+        {
+            RuleFor(p => p.Name)
+                .NotEmpty().WithMessage("You must enter Sample Name")
+                .MaximumLength(50).WithMessage("Sample Name cannot be longer than 50 characters");
+
+            RuleFor(p => p.LastName)
+                .NotEmpty().WithMessage("You must enter Sample LastName")
+                .MaximumLength(250).WithMessage("Sample LastName cannot be longer than 250 characters");
+        }
     }
 }
