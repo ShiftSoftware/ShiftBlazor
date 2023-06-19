36128ca8bdb15c157245efed6975c297a1249351
Saman <gtsaman@gmail.com>
2023-06-19 15:49:50 +0300

    Remove ShiftIdentity package, use a local User object to parse the data in RevisionViewer

---
+++ ShiftBlazor/Components/RevisionViewer/UserDetails.cs
@@ -0,0 +1,17 @@
+﻿using ShiftSoftware.ShiftEntity.Model.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShiftSoftware.ShiftBlazor.Components
+{
+    public class UserDetails : ShiftEntityListDTO
+    {
+        [ShiftEntity.Model.HashId.UserHashIdConverter]
+        public override string? ID { get; set; }
+
+        public string Name { get; set; }
+    }
+}
