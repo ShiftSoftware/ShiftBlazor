c6e6a8bc8c8531af5425b1c5b985c3a364afdc4b
Saman <gtsaman@gmail.com>
2023-09-26 16:22:54 +0300

    #861n9r34x: Add Tests Viewer project

--- ShiftBlazor.Tests.Viewer/Program.cs
+++ ShiftBlazor.Tests.Viewer/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using ShiftBlazor.Tests.Viewer;
+using ShiftSoftware.ShiftBlazor.Extensions;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -8,4 +9,20 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-await builder.Build().RunAsync();
+var baseUrl = builder.Configuration!.GetValue<string>("BaseURL")!;
+
+builder.Services.AddShiftBlazor(config =>
+{
+    config.ShiftConfiguration = options =>
+    {
+        options.BaseAddress = baseUrl!;
+        options.ApiPath = "/api";
+        options.ODataPath = "/odata";
+        options.UserListEndpoint = baseUrl.AddUrlPath("odata/IdentityPublicUser"); //ToDo: this parameter should be optional.
+        options.AddLanguage("en-US", "English")
+               .AddLanguage("en-US", "English RTL", true);
+    };
+});
+
+var app = builder.Build();
+await app.RunAsync();
