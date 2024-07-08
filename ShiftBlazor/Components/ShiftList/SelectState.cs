aa49505d716e136a15ef4d3de9efba0263ac8bf1
Saman <gtsaman@gmail.com>
2024-07-08 23:35:46 +0300

    Inherit SelectStateDTO and add Clear method

---
+++ ShiftBlazor/Components/ShiftList/SelectState.cs
@@ -0,0 +1,14 @@
+﻿using ShiftSoftware.ShiftEntity.Model.Dtos;
+
+
+namespace ShiftSoftware.ShiftBlazor.Components;
+
+public class SelectState<T> : SelectStateDTO<T>
+{
+    public void Clear()
+    {
+        Items.Clear();
+        All = false;
+        Filter = null;
+    }
+}
