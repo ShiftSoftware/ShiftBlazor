ed4e8be5ba8fa25e5e146ec0d23a9e1db128c153
Saman-00 <gtsaman@gmail.com>
2024-08-26 18:49:57 +0300

    Add some options to UserAvatar component

---
+++ ShiftBlazor/Components/UserAvatar/UserAvatar.razor.cs
@@ -0,0 +1,53 @@
+﻿using ShiftSoftware.ShiftIdentity.Core.DTOs;
+using ShiftSoftware.ShiftIdentity.Core;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using ShiftSoftware.ShiftIdentity.Blazor;
+
+namespace ShiftSoftware.ShiftBlazor.Components;
+
+public partial class UserAvatar
+{
+    [Inject] IDialogService Dialog { get; set; } = default!;
+    [Inject] IIdentityStore tokenStore { get; set; } = default!;
+    [Inject] NavigationManager NavigationManager { get; set; } = default!;
+
+    [Parameter]
+    public MouseEvent ActivationEvent { get; set; } = MouseEvent.LeftClick;
+
+    [Parameter]
+    public RenderFragment? MenuItemsTemplate { get; set; }
+
+    [Parameter]
+    public bool ShowClosedOpenIcon { get; set; }
+
+    [Parameter]
+    public string? IconClosed { get; set; } = Icons.Material.Filled.KeyboardArrowDown;
+
+    [Parameter]
+    public string? IconOpen { get; set; } = Icons.Material.Filled.KeyboardArrowUp;
+
+    public bool IsOpen = false;
+    
+    TokenUserDataDTO? userdata;
+
+    protected override async Task OnInitializedAsync()
+    {
+        userdata = (await tokenStore.GetTokenAsync())?.UserData;
+    }
+
+    internal async Task Logout()
+    {
+        await tokenStore.RemoveTokenAsync();
+        NavigationManager.NavigateTo("/", true);
+    }
+
+    internal void OpenSettings() => Dialog.Show<Settings>();
+
+    internal void IsOpenHandler(bool open) => IsOpen = open;
+
+    internal void GoToIdentity()
+    {
+        NavigationManager.NavigateTo($"{IdentityOptions.FrontEndBaseUrl}/{Constants.IdentityRoutePreifix}/UserDataForm");
+    }
+}
