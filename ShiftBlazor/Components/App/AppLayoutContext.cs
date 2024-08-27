14bbcc09200f25ec5f0ca1e9dc147d36139e8660
Saman <gtsaman@gmail.com>
2024-08-27 13:57:00 +0300

    Move MainLayout to Layouts namespace and add new empty layout

--- ShiftBlazor/Components/App/AppLayoutContext.cs
+++ ShiftBlazor/Components/App/AppLayoutContext.cs
@@ -6,5 +6,7 @@ public class AppLayoutContext
 {
     public string? Title { get; set; }
     public RenderFragment<NavMenuContext>? NavMenuTemplate { get; set; }
+    public RenderFragment? HeaderTemplate { get; set; }
+    public RenderFragment? FooterTemplate { get; set; }
     public bool EnableAutherization { get; set; }
 }
---
+++ ShiftBlazor/Layouts/EmptyLayout.razor.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Components.Routing;
+using Microsoft.AspNetCore.Components;
+using ShiftSoftware.ShiftBlazor.Services;
+using Microsoft.JSInterop;
+using ShiftSoftware.ShiftBlazor.Interfaces;
+using ShiftSoftware.ShiftBlazor.Enums;
+using ShiftSoftware.ShiftBlazor.Components;
+
+namespace ShiftSoftware.ShiftBlazor.Layouts;
+
+public partial class EmptyLayout : LayoutComponentBase
+{
+    [Inject] private ShiftModal ShiftModal { get; set; } = default!;
+    [Inject] private NavigationManager NavManager { get; set; } = default!;
+    [Inject] private SettingManager SettingManager { get; set; } = default!;
+
+    [CascadingParameter]
+    public AppLayoutContext? App { get; set; }
+
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+
+        NavManager.LocationChanged += HandleLocationChanged;
+        OpenModals();
+    }
+
+    [JSInvokable]
+    public static void SendKeys(IEnumerable<string> keys)
+    {
+        _ = IShortcutComponent.SendKeys(keys.Select(Enum.Parse<KeyboardKeys>));
+    }
+
+    private void HandleLocationChanged(object? sender, LocationChangedEventArgs e)
+    {
+        OpenModals();
+    }
+
+    private void OpenModals()
+    {
+        ShiftModal.UpdateModals();
+    }
+
+}
