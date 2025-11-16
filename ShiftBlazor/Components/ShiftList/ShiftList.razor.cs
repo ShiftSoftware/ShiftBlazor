ed29c7e7bd3b951e7bab7e833b5d27f2458a62c5
AzaAsim <aza@shift.software>
2025-11-16 21:28:02 +0300

    ShiftList: use ForceExportIfHidden

--- ShiftBlazor/Components/ShiftList/ShiftList.razor.cs
+++ ShiftBlazor/Components/ShiftList/ShiftList.razor.cs
@@ -1224,6 +1224,16 @@ public partial class ShiftList<T> : IODataRequestComponent<T>, IShortcutComponen
 
                     // don't export column if the column is hidden or is readonly with no CustomColumnExport attr
                     var isHidden = x.Hidden || attr?.Hidden == true || (property?.CanWrite == false && customColumn == null);
+
+                    if (
+                        x.GetType().GetProperty(nameof(PropertyColumnExtended<T, object>.ForceExportIfHidden)) is PropertyInfo propertyInfo &&
+                        propertyInfo.GetValue(x) is bool forceExport &&
+                        forceExport
+                    )
+                    {
+                        isHidden = false;
+                    }
+
                     var type = propType?.Name;
                     
                     if (property?.GetCustomAttribute<JsonConverterAttribute>()?.ConverterType == typeof(LocalizedTextJsonConverter))
