917e743927b24e51b2446810ba437f9c99c52849
AzaAsim <aza@shift.software>
2026-01-06 21:46:57 +0300

    Add TransformedBrowserFile to be used for mutating the IBrowserFile? LocalFile in UploaderItem

---
+++ ShiftBlazor/Utils/TransformedBrowserFile.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+
+namespace ShiftSoftware.ShiftBlazor.Utils;
+
+public class TransformedBrowserFile(byte[] content, string name, string contentType, DateTimeOffset lastModified)
+    : IBrowserFile
+{
+    public string Name => name;
+    public DateTimeOffset LastModified => lastModified;
+    public long Size => content.Length;
+    public string ContentType => contentType;
+
+    public Stream OpenReadStream(long maxAllowedSize = 512000L, CancellationToken cancellationToken = default)
+    {
+        if (Size > maxAllowedSize)
+            throw new IOException($"File size exceeds maximum allowed size of {maxAllowedSize} bytes.");
+
+        return new MemoryStream(content);
+    }
+}
