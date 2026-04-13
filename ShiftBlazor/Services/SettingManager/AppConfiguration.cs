5203dcb3292531b79a8e91d8491c2fe6258dbe3a
Saman-00 <gtsaman@gmail.com>
2026-04-13 20:39:21 +0300

    Separate UI and Content Language

--- ShiftBlazor/Services/SettingManager/AppConfiguration.cs
+++ ShiftBlazor/Services/SettingManager/AppConfiguration.cs
@@ -1,4 +1,6 @@
-﻿namespace ShiftSoftware.ShiftBlazor.Services;
+﻿using ShiftSoftware.ShiftBlazor.Enums;
+
+namespace ShiftSoftware.ShiftBlazor.Services;
 
 public class AppConfiguration
 {
@@ -8,12 +10,6 @@ public class AppConfiguration
     public string? UserListEndpoint { get; set; }
 
 
-    //public string ODataPath
-    //{
-    //    get => BaseAddress.AddUrlPath(_ODataPath);
-    //    set => _ODataPath = value;
-    //}
-
     /// <summary>
     /// A list of thumbnail sizes to generate when a new image is uploaded.
     /// Width x Height format.
@@ -21,9 +17,17 @@ public class AppConfiguration
     public List<ValueTuple<int, int>> ThumbnailSizes = [(250, 250), (500, 500), (1000, 1000)];
 
     public List<LanguageInfo> Languages = [];
+    public List<LanguageInfo> ContentLanguages = [];
 
-    public AppConfiguration AddLanguage(string culture, string label, bool rtl = false)
+
+    public AppConfiguration AddLanguage(string culture, string label, LanguageScope scope)
     {
+        return this.AddLanguage(culture, label, false, scope);
+    }
+
+    public AppConfiguration AddLanguage(string culture, string label, bool rtl = false, LanguageScope scope = LanguageScope.Both)
+    {
+        //NullReferenceException.ThrowIfNull(culture, nameof(culture));
         if (string.IsNullOrWhiteSpace(culture)) throw new NullReferenceException(nameof(culture));
 
         if (string.IsNullOrWhiteSpace(label))
@@ -31,12 +35,25 @@ public class AppConfiguration
             label = culture;
         }
 
-        Languages.Add(new LanguageInfo
+        if (scope.HasFlag(LanguageScope.UI))
         {
-            CultureName = culture,
-            Label = label,
-            RTL = rtl
-        });
+            Languages.Add(new LanguageInfo
+            {
+                CultureName = culture,
+                Label = label,
+                RTL = rtl
+            });
+        }
+
+        if (scope.HasFlag(LanguageScope.Content))
+        {
+            ContentLanguages.Add(new LanguageInfo
+            {
+                CultureName = culture,
+                Label = label,
+                RTL = rtl
+            });
+        }
 
         return this;
     }
